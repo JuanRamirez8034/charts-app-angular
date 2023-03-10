5ec5f59bf0b348980ecdfd127aa7f6cb6f3d4670
Juan de Dios Ramirez Lozano <juan.ramirez@adm.urbe.edu>
2023-03-10 15:27:12 -0400

    Terminada la grafica doughnut con peticion http

--- src/app/charts/pages/doughnut-http/doughnut-http.component.ts
+++ src/app/charts/pages/doughnut-http/doughnut-http.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { ChartsService } from '../../services/charts.service';
 
 @Component({
   selector: 'app-doughnut-http',
   templateUrl: './doughnut-http.component.html',
   styleUrls: ['./doughnut-http.component.css']
 })
-export class DoughnutHttpComponent {
+export class DoughnutHttpComponent implements OnInit{
+
+  public data !: {rs:string[],rsNumbr:string[]};
+
+  public doughnutChartData !: ChartData<'doughnut'> ;
+
+  public chartOptions : ChartConfiguration['options'] = {
+    responsive:true
+  }
+
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor(private chartService:ChartsService){}
+
+
+  ngOnInit(): void {
+
+    this.chartService.getNumberRsUsers()
+    .subscribe(data=>{
+    
+    this.data = {
+      rs: Object.keys(data),
+      rsNumbr: Object.values(data)
+    };
+
+    console.log('Cargando data Servidor', this.data);
+    
+    console.log('Cargando la grafica');
+    
+    this.doughnutChartData = {
+      labels: this.data?.rs,
+      datasets: [
+        {data: this.data.rsNumbr.map(e => parseInt(e))}
+      ]
+    }
+
+   },
+   error=>{
+    console.log('Hubo un error al traer la data',error);
+    
+   });
+    
+  }
+
+
+
 
 }
--- src/app/charts/services/charts.service.ts
+++ src/app/charts/services/charts.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartsService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  public getNumberRsUsers(){
+    return this.http.get<any>('http://localhost:3000/grafica').pipe(
+      delay(2000),
+      map(resp => (resp))
+    );
+  }
 }
